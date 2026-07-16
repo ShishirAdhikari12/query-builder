7c45f93259664028f97186857f9e96bf91775b68
Shishir Adhikari <shishiradhikari1829@gmail.com>
2026-07-16 23:03:05 +0545

    practice whereIn, whereBy, whereBetween, whereNotBetween, orWhere etc.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,53 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    /* 
+    select * from actor 
+    where last_name = 'Berry'
+    */
+
+    // $actors = DB::table('actor')
+    //     ->where('last_name', '=', 'Berry')
+    //     ->where('first_name', 'Karl')
+    //     ->get();
+
+    // $actors = DB::table('actor')
+    //     ->where([
+    //         ['last_name', 'Berry'],
+    //         ['first_name', 'Karl']
+    //     ])
+    //     ->get();
+
+    // $actors = DB::table('actor')
+    //     ->where(function ($query) {
+    //         $query->where([
+    //             ['last_name', '=', 'Berry'],
+    //             ['first_name', 'Karl']
+    //         ]);
+    //     })
+    //     ->get();
+
+// ----------------------------------------------------------------------------------------------------------
+    # list the last names of actors, count the number of times last name is being shared
+    /* 
+    select last_name, count(*) as actor_count
+    from actor
+    group by last_name
+    order by actor_count desc
+    */
+
+
+    $actors = DB::table('actor')
+    ->select(['last_name', DB::raw('count(*) as actor_count')])
+    ->groupBy('last_name')
+    ->orderBy('actor_count', 'desc')
+    ->get();
+
+// ------------------------------------------------------------------------------------------------------
+
+    return $actors;
 });
